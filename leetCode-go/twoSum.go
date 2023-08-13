89cfca917cda64c75cb030d2cff5a4338d4423d7
Catherine Lim <cathlim.98@gmail.com>
2023-08-13 23:38:26 +0800

    Add twoSum.go

---
+++ leetCode-go/twoSum.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(nums []int, target int) []int {
+	var indices = make(map[int]int)
+
+	for i, value := range nums{
+		x := target - value;
+
+		found, ok := indices[x]
+		if ok {
+			result := []int {found, i}
+			return result
+		}
+		indices[value] = i
+	}
+	return nil
+
+}
